0152f805eba2e16e594b7c0681b86d4a8f5038e0
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2024-01-04 23:53:18 +0700

    feat: add register api

--- backend/authentification/views.py
+++ backend/authentification/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
+from django.contrib.auth.models import User
 
 class HomeView(APIView):
    permission_classes = (IsAuthenticated, )
@@ -25,3 +26,15 @@ class LogoutView(APIView):
           return Response(status=status.HTTP_205_RESET_CONTENT, headers={'Access-Control-Allow-Origin': 'http://localhost:5173'})
         except Exception as e:
           return Response(status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': 'http://localhost:5173'})
+
+class RegisterView(APIView):
+   permission_classes = ()
+   def post(self, request):
+      try:
+        username = request.data['username']
+        password = request.data['password']
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        return Response(status=status.HTTP_201_CREATED, headers={'Access-Control-Allow-Origin': 'http://localhost:5173'})
+      except Exception as e:
+        return Response(status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': 'http://localhost:5173'})
